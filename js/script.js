f9a896d08b05156b582c06f44b8fe2fc5567803c
Nischal Rai <nischalpilmong@yahoo.com>
2020-03-20 11:37:32 +0100

    initial Get Request done

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//Searchbar handler
+$(function(){
+   var searchField = $('#query');
+   var icon = $('#search-btn');
+
+   //Focus Handler
+    $(searchField).on('focus',function(){
+       $(this).animate({
+         width: '100%'
+       },400);
+       $(icon).animate({
+          right: '10px'
+       },400);
+    });
+    //Blur Event Handler
+    $(searchField).on('blur',function(){
+        if(searchField.val() == ''){
+            $(searchField).animate({
+                width: '45%'
+            },400,function(){});
+            $(icon).animate({
+                right: '360px'
+            },400,function(){});
+        }
+
+    });
+    $('#search-form').submit(function(event){
+       event.preventDefault();
+    });
+});
+
+function search(){
+    //Clear Results
+     $('#results').html('');
+     $('#buttons').html('');
+
+     //Get Form Input
+    q = $('#query').val();
+
+    //Run GET Request on API
+    $.get(
+      "https://www.googleapis.com/youtube/v3/search",{
+          part: 'snippet,id',
+          q: q,
+          type: 'video',
+          key: 'AIzaSyClBCsSgVv0FcTCzkUSdlpTukBlK47e4fA'},
+        function(data){
+          var nextPageToken = data.nextPageToken;
+          var prevPageToken = data.prevPageToken;
+
+          //Log Data
+          console.log(data);
+         $.each(data.items, function(i,item){
+            //Get Output
+            var output = getOutput(item);
+
+            //Display Results
+             $('#results').append(output);
+
+
+         });
+
+
+        }
+
+    );
+}
